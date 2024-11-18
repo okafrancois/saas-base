db633c20f41c5dc86d5108c97196354f01b7d8b8
okacoder <admin@okafrancois.dev>
2024-11-18 19:29:55 +0100

    add menu

--- src/components/nav-main.tsx
+++ src/components/nav-main.tsx
@@ -20,12 +20,20 @@ import {
 } from "@/components/ui/sidebar"
 import { useTranslations } from 'next-intl'
 import { NavItem } from '@/components/layouts/types'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export function NavMain({
   items,
 }: {
   items: NavItem[]
 }) {
+  const path = usePathname()
+
+  function isActive(href: string) {
+    return path === href
+  }
+
   const t = useTranslations("navigation")
   return (
     <SidebarGroup>
@@ -36,11 +44,11 @@ export function NavMain({
         {items.map((item) => (
           <Collapsible key={item.title} asChild defaultOpen={item.isActive}>
             <SidebarMenuItem>
-              <SidebarMenuButton asChild tooltip={item.title}>
-                <a href={item.href}>
+              <SidebarMenuButton isActive={isActive(item.href)} asChild tooltip={item.title}>
+                <Link href={item.href}>
                   {item.icon}
                   <span>{item.title}</span>
-                </a>
+                </Link>
               </SidebarMenuButton>
               {item.items?.length ? (
                 <>
@@ -55,9 +63,9 @@ export function NavMain({
                       {item.items?.map((subItem) => (
                         <SidebarMenuSubItem key={subItem.title}>
                           <SidebarMenuSubButton asChild>
-                            <a href={subItem.href}>
+                            <Link href={subItem.href}>
                               <span>{subItem.title}</span>
-                            </a>
+                            </Link>
                           </SidebarMenuSubButton>
                         </SidebarMenuSubItem>
                       ))}
